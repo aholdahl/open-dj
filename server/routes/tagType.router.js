24ba026010e3252d532c7c591f276bb8219ecea3
Allyson Holdahl <allyson.holdahl@gmail.com>
2021-10-24 17:21:57 -0500

    add server pool and tagType route

---
+++ server/routes/tagType.router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const pool = require('../modules/pool.js');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM tag_types ORDER BY "type" ASC;`
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+        })
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    let queryText = `INSERT INTO "tag_types" ("type") VALUES ($1);`
+    pool.query(queryText, [req.body.type])
+        .then((result) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+})
+
+router.put('/', (req, res) => {
+    let queryText = `UPDATE "tag_types" SET "type" = $1 WHERE "id" = $2;`
+    pool.query(queryText, [req.body.type, req.body.id])
+        .then((result) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+})
+
+router.delete('/:id', (req, res) => {
+    let queryText = `DELETE FROM "tag_types" WHERE "id" = $1;`
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Route includes
+const tagTypeRouter = require('./routes/tagType.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('build'));
+
+/* Routes */
+app.use('/tagType', tagTypeRouter);
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
